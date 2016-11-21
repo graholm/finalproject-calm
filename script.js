7e8e1b9be1e397f5116cc88f2a39013a26762d78
graholm <graholm@nuevaschool.org>
2016-11-21 09:10:33 -0800

    Classadoo autocommit

--- script.js
+++ script.js
@@ -1,7 +1,19 @@
-<script>
-var images = ['http://img12.deviantart.net/627f/i/2005/026/e/2/calm_sea_by_ladyrapid.jpg', 'https://static.pexels.com/photos/1826/wood-nature-sunny-forest.jpg', 'http://thirstypeople.org/wp-content/uploads/2016/02/calm_water-1.jpg', 'http://kingofwallpapers.com/calm-pictures/calm-pictures-022.jpg'];
-
-
-document.getElementsByClassName('mainview')[0].style.backgroundImage = 'url(' + images[Math.round(Math.random() * images.length)] + ')';
-
+<html>
+  <head>
+  <script type="text/javascript"> 
+    var totalCount = 8;
+function ChangeIt() 
+{
+  var num = Math.ceil( Math.random() * totalCount );
+  document.body.background = 'bgimages/'+num+'.jpg';
+  document.body.style.backgroundRepeat = "repeat";// Background repeat
+}
 </script>
+  </head>
+  <body> 
+  // Page Design 
+  </body> 
+  <script type="text/javascript"> 
+    ChangeIt();
+</script> 
+  </html>
